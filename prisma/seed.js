3cb1247eb9a1e513e7c1de31ed93fdb5a2770117
Oğuzhan Osma <osmaoguzhan33@gmail.com>
2023-04-20 16:08:01 +0200

    TICARE-T-47: Fixed companyId assignment

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,44 +2,9 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  const [country] = await prisma.country.findMany({
-    where: {
-      name: "Turkey",
-    },
-  });
-  const osma = await prisma.user.create({
-    data: {
-      name: "Oguzhan",
-      surname: "Osma",
-      email: "osmaoguzhan@outlook.com",
-      password: "123456",
-      role: "USER",
-      phoneNumber: "123456789",
-      settings: {
-        language: "gb",
-        currency: "USD",
-        timezone: "UTC",
-        theme: "light",
-      },
-    },
-  });
-  const company = await prisma.company.create({
-    data: {
-      name: "Company",
-      description: "Company Description",
-      address: "Company Address",
-      city: country.states[0].name,
-      country: country.name,
-      zipCode: "Company Zip Code",
-      phoneNumber: "Company Phone Number",
-      email: "Company Email",
-      website: "Company Website",
-      ownerId: osma.id,
-    },
-  });
-  // console.log(turkey.states[0].name);
-  console.log(osma);
-  console.log(company);
+  let email = "osmaoguzhan@outlook.com";
+  let user = await prisma.user.findFirst({ where: { email } });
+  console.log(user);
 }
 
 main()
